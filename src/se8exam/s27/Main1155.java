127b0e7c6d57acd87be457b0d1114fae74e536ee
Toshiro Miyamoto <toshiro.en.miyamoto@gamil.com>
2019-08-06 17:28:56 +0900

    Silver 27

---
+++ src/se8exam/s27/Main1155.java
@@ -0,0 +1,9 @@
+package se8exam.s27;
+public class Main1155 {
+   public static void main(String[] args) {
+      boolean flgA = (5.0 != 6.0) && (4 != 5);
+      boolean flgB = (4 != 4) || (4 == 4);
+      System.out.println(flgA);
+      System.out.println(flgB);
+   }
+}
